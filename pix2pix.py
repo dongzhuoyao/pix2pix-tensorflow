1c283a8d1386c3889b6474c4937d3b908c603212
dongzhuoyao <929109550@qq.com>
2017-04-02 20:51:40 +0800

    t

--- pix2pix.py
+++ pix2pix.py
@@ -312,8 +312,8 @@ def load_examples1():
             r = tf.image.random_flip_left_right(r, seed=seed)
 
         #maybe buggy
-        offset_h = tf.random_uniform([1], 0, h - CROP_SIZE + 1, seed=seed,dtype=tf.int32)
-        offset_w = tf.random_uniform([1], 0, w - CROP_SIZE + 1, seed=seed,dtype=tf.int32)
+        offset_h = tf.cast(tf.floor(tf.random_uniform([1], 0, h - CROP_SIZE + 1, seed=seed)), dtype=tf.int32)
+        offset_w = tf.cast(tf.floor(tf.random_uniform([1], 0, w - CROP_SIZE + 1, seed=seed)), dtype=tf.int32)
         print(r)
         print("shit")
         print(offset_h)
