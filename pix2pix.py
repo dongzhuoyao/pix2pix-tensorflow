e2cb6c75d62106ae9024b455ece2847a0f0d7e43
dongzhuoyao <929109550@qq.com>
2017-04-03 21:08:25 +0800

    t

--- pix2pix.py
+++ pix2pix.py
@@ -12,6 +12,18 @@ import random
 import collections
 import math
 import time
+#http://stackoverflow.com/questions/107705/disable-output-buffering
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+import sys
+sys.stdout = Unbuffered(sys.stdout)
 
 parser = argparse.ArgumentParser()
 
