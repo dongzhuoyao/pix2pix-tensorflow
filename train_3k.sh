09c5bacf58df9d14f72fbd3fa2db67203b1b6883
dongzhuoyao <929109550@qq.com>
2017-04-03 20:44:14 +0800

    t

--- train_3k.sh
+++ train_3k.sh
@@ -6,4 +6,5 @@ CUDA_VISIBLE_DEVICES=1 python pix2pix.py \
 		    --d_train_dir   /home/hutao1/dresult/normal_cityscapes/train \
             --d_train_gt_dir /home/hutao1/dresult/normal_cityscapes/train_5 \
             --d_img_height 1024 \
-            --d_img_width 2048
+            --d_img_width 2048 \
+            --batch_size 16
