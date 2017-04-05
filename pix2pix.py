fef8be5a617c368aeaa95bf68306316fb5c66837
dongzhuoyao <929109550@qq.com>
2017-04-06 00:44:56 +0800

    t

--- pix2pix.py
+++ pix2pix.py
@@ -13,6 +13,19 @@ import collections
 import math
 import time
 
+#http://stackoverflow.com/questions/107705/disable-output-buffering
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+import sys
+sys.stdout = Unbuffered(sys.stdout)
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--d_train_list", help="path to folder containing images")
